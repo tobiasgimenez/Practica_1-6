d9a8966d28089c138d26f6a8a93c22ac27ec886c
tobias gimenez~ <tobiasgimenezchirino@gmail.com~>
2022-04-20 12:11:37 -0300

    agregue index y algoritmo.js

---
+++ algoritmo.js
@@ -0,0 +1,14 @@
+var respuesta = prompt("Ingrese su apodo", ["apodo"]);//escribe mediante el navegador
+
+console.log( respuesta );// escribe algo mediante la consola
+
+document.write(`
+    <h4>Apodo: </h4>
+    <h4> ${ respuesta }</h4>
+`)
+
+// defomp una variable llamada "hola" y la inicializo con un valor literal de "el tipo de dato que use"
+var string = "hola";
+var numero = 3;
+var decimal = 1.5;
+var booleano= false;
